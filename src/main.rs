2ea87b5e5c0f965fe86f290dc4e73f8f2fa7a9af
Dimas Cyriaco <dimascyriaco@pm.me>
2022-11-05 14:05:24 -0300

    feat: load resources and buildings from file

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,94 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs, process::exit};
 
-#[macro_use]
-extern crate lazy_static;
-
-lazy_static! {
-    static ref RESOURCES: HashMap<&'static str, Resource> = {
-        let mut m: HashMap<&'static str, Resource> = HashMap::new();
-
-        m.insert(
-            "wood",
-            Resource {
-                name: String::from("Wood"),
-            },
-        );
-        m
-    };
-}
-
-lazy_static! {
-    static ref BUILDINGS: HashMap<&'static str, Building<'static>> = {
-        let mut buildings: HashMap<&'static str, Building> = HashMap::new();
-
-        let wood = RESOURCES.get("wood").unwrap();
-
-        buildings.insert(
-            "wood_cutter",
-            Building {
-                name: String::from("Wood Cutter"),
-                inputs: vec![],
-                outputs: vec![Output {
-                    resource: wood,
-                    quantity: 5,
-                }],
-            },
-        );
-
-        buildings
-    };
-}
+use serde::Deserialize;
 
 fn main() {
+    let resources_filename = "data/resources.toml";
+
+    let resources_file_content = match fs::read_to_string(resources_filename) {
+        // If successful return the files text as `contents`.
+        // `c` is a local variable.
+        Ok(c) => c,
+        // Handle the `error` case.
+        Err(_) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Could not read file `{}`", resources_filename);
+            // Exit the program with exit code `1`.
+            exit(1);
+        }
+    };
+
+    let resources: HashMap<&str, Resource> = match toml::from_str(&resources_file_content) {
+        // If successful, return data as `Data` struct.
+        // `d` is a local variable.
+        Ok(d) => d,
+        // Handle the `error` case.
+        Err(_) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Unable to load data from `{}`", resources_filename);
+            // Exit the program with exit code `1`.
+            exit(1);
+        }
+    };
+
+    let buildings_filename = "data/buildings.toml";
+
+    let buildings_file_content = match fs::read_to_string(buildings_filename) {
+        // If successful return the files text as `contents`.
+        // `c` is a local variable.
+        Ok(c) => c,
+        // Handle the `error` case.
+        Err(_) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Could not read file `{}`", buildings_filename);
+            // Exit the program with exit code `1`.
+            exit(1);
+        }
+    };
+
+    let buildings: HashMap<&str, Building> = match toml::from_str(&buildings_file_content) {
+        // If successful, return data as `Data` struct.
+        // `d` is a local variable.
+        Ok(d) => d,
+        // Handle the `error` case.
+        Err(e) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Unable to load data from `{}`", buildings_filename);
+            dbg!(e);
+            // Exit the program with exit code `1`.
+            exit(1);
+        }
+    };
+
     let deficit = 10;
 
-    let suggestions: Vec<Suggestion> = get_suggestions("wood", deficit);
+    let suggestions: Vec<Suggestion> = get_suggestions("wood", deficit, &resources, &buildings);
 
     dbg!(suggestions);
 }
 
-fn get_suggestions<'a>(resource_name: &str, deficit: u32) -> Vec<Suggestion<'a>> {
-    let wood = RESOURCES.get(resource_name).unwrap();
+fn get_suggestions<'a>(
+    resource_id: &str,
+    deficit: u32,
+    resources: &'a HashMap<&str, Resource>,
+    buildings: &'a HashMap<&str, Building>,
+) -> Vec<Suggestion<'a>> {
+    let wood = resources.get(resource_id).unwrap();
 
-    BUILDINGS
+    buildings
         .iter()
         .filter(|(_, building)| {
-            building
-                .outputs
-                .iter()
-                .any(|output| output.resource == wood)
+            building.outputs.iter().any(|output| {
+                dbg!(output.resource_id.clone());
+                dbg!(resource_id);
+                output.resource_id == resource_id
+            })
         })
         .map(|(_, building)| {
             let output_quantity = building
                 .outputs
                 .iter()
-                .find(|output| output.resource == wood)
+                .find(|output| output.resource_id == *resource_id)
                 .map_or(0, |o| o.quantity);
 
             let quantity = deficit / output_quantity;
@@ -81,30 +107,30 @@ fn get_suggestions<'a>(resource_name: &str, deficit: u32) -> Vec<Suggestion<'a>>
 struct Suggestion<'a> {
     deficit: u32,
     target: &'a Resource,
-    solution: &'a Building<'a>,
+    solution: &'a Building,
     quantity: u32,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Deserialize)]
 struct Resource {
     name: String,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Deserialize)]
 struct Input {
-    resource: Resource,
+    resource_id: String,
     quantity: u32,
 }
 
-#[derive(Debug, PartialEq)]
-struct Output<'a> {
-    resource: &'a Resource,
+#[derive(Debug, PartialEq, Deserialize)]
+struct Output {
+    resource_id: String,
     quantity: u32,
 }
 
-#[derive(Debug, PartialEq)]
-struct Building<'a> {
+#[derive(Debug, PartialEq, Deserialize)]
+struct Building {
     name: String,
     inputs: Vec<Input>,
-    outputs: Vec<Output<'a>>,
+    outputs: Vec<Output>,
 }
